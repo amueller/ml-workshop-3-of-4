babc8d4ea41f08416c746243154175f66bf02351
Andreas Mueller <andreas.mueller@columbia.edu>
2020-04-17 14:00:10 -0400

    use value_counts(normalize=True), update sklearn doc screenshot

--- notebooks/solutions/bank_campaign.py
+++ notebooks/solutions/bank_campaign.py
@@ -10,7 +10,7 @@ X = data.drop("target", axis=1)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
 
-print("label proportions:\n", y.value_counts() / len(y))
+print("label proportions:\n", y.value_counts(normalize=True))
 
 
 
